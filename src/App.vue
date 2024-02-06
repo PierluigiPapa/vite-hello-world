fd436d1c8fc240922ef94de388cfd2826cd19ae7
PierluigiPapa <pierluigipapa9@gmail.com>
2024-02-06 16:03:31 +0100

    feat: add jumbotron

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script>
 import AppTitle from './components/AppTitle.vue';
+import AppHero from './components/AppHero.vue';
 
   export default{
     components:{
       AppTitle,
+      AppHero,
      }
   }
 
@@ -11,6 +13,7 @@ import AppTitle from './components/AppTitle.vue';
 
 <template>
 <AppTitle></AppTitle>
+<AppHero></AppHero>
 </template>
 
 <style>
---
+++ src/components/AppHero.vue
@@ -0,0 +1,38 @@
+<script>
+export default {
+  name: 'AppHero'
+}
+</script>
+
+<template>
+<div class="jumbotron">
+    <h2>Buon festival di Sanremo!</h2>
+    <img src="/images/Amadeus_Sanremo.jpg" alt="">
+    <p>E soprattutto... BUON FANTA SANREMO!</p>
+    
+  </div>
+</template>
+
+<style scoped>
+
+.jumbotron {
+    text-align: center;
+    padding: 15px;
+    margin-bottom: 15px;
+    border-radius: 20px;
+    color: white;
+}
+
+img {
+    padding-top: 20px;
+    width: 50%;
+    height: 50%;
+}
+
+p {
+    padding-top: 20px;
+    font-weight: bold;
+    font-size: 20px;
+}
+
+</style>
